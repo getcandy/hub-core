22788a5fbce7f4c7e053c76c312fd95225a37f4a
Alec <hello@itsalec.co.uk>
2021-01-13 13:09:14 +0000

    Remove storybook bits

--- hub-ui/.storybook/main.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  stories: ['../stories/**/**/*.stories.(js|mdx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-knobs/register'],
-};
-
--- hub-ui/.storybook/postcss.config.js
+++
@@ -1,9 +0,0 @@
-var tailwindcss = require('tailwindcss');
-
-module.exports = {
-  plugins: [
-    require('postcss-import')(),
-    tailwindcss('tailwind.config.js'), //This refers to your tailwind config
-    require('autoprefixer'),
-  ],
-};
