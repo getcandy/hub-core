6fa83b69755e03399735c8b2c99672eb45fe51e9
Alec <hello@itsalec.co.uk>
2021-01-13 13:06:45 +0000

    Add some ui styles

--- hub-ui/src/components/Table.vue
+++ hub-ui/src/components/Table.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="w-full  overflow-y-scroll">
+  <div class="w-full overflow-y-scroll">
     <table class="w-full" >
       <thead>
         <tr>
           <th v-if="sortable.handle" />
-          <th v-for="(column, index) in columns" :key="index" class="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+          <th v-for="(column, index) in columns" :key="index" class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-600 uppercase bg-gray-100">
             {{ column.label }}
           </th>
         </tr>
       </thead>
       <template v-if="state === 'loading'">
         <tfoot>
-          <tr class="bg-white text-center">
-            <td colspan="50" class="px-6 py-4 whitespace-no-wrap text-xs uppercase font-medium text-gray-500 text-center">
-              <span class="flex items-center mx-auto justify-center">
+          <tr class="text-center bg-white">
+            <td colspan="50" class="px-6 py-4 text-xs font-medium text-center text-gray-500 uppercase whitespace-no-wrap">
+              <span class="flex items-center justify-center mx-auto">
                 <gc-icon icon="rotate-clockwise" spin/>
                 <span class="ml-2">{{ $t('Loading') }}</span>
               </span>
@@ -25,14 +25,14 @@
         <slot name="tfoot" />
 
         <tfoot v-if="(!data || !data.length) && !$slots.tfoot">
-          <tr class="bg-white text-center">
-            <td colspan="50" class="px-6 py-4 whitespace-no-wrap text-xs uppercase font-medium text-gray-500 text-center">No records</td>
+          <tr class="text-center bg-white">
+            <td colspan="50" class="px-6 py-4 text-xs font-medium text-center text-gray-500 uppercase whitespace-no-wrap">No records</td>
           </tr>
         </tfoot>
         <slot name="tbody" v-bind="data" />
         <tbody v-if="!$slots.tbody" v-sortable="sortable">
           <tr :class="{'bg-white': index % 2 === 0, 'bg-gray-100': index % 2 !== 0}" v-for="(row, index) in data" :key="index">
-            <td v-if="sortable.handle" :class="sortableHandle" class="px-6 py-4 w-2 cursor-grab">
+            <td v-if="sortable.handle" :class="sortableHandle" class="w-2 px-6 py-4 cursor-grab">
               <svg width="13px" viewBox="0 0 13 19" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                   <g fill="#D8D8D8">
@@ -46,17 +46,21 @@
                 </g>
               </svg>
             </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm font-medium text-gray-900" v-for="(column, colIndex) in columns" :key="colIndex">
-              <!-- <slot v-bind:
-                -->
-                <slot :name="column.field" v-bind="{ row, index }"></slot>
+            <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-no-wrap" v-for="(column, colIndex) in columns" :key="colIndex">
+                <template v-if="column.render">
+                  {{ column.render(row) }}
+                </template>
+                <template v-if="column.component">
+                  <component :is="column.component" :row="row" />
+                </template>
+                <slot :name="column.field" v-bind="{ row, index }" v-else></slot>
             </td>
           </tr>
         </tbody>
       </template>
     </table>
     <template v-if="state != 'loading'">
-      <nav class="border-t border-gray-200 px-4 flex items-center justify-center sm:px-0 pb-3" v-if="meta && meta.links">
+      <nav class="flex items-center justify-center px-4 pb-3 border-t border-gray-200 sm:px-0" v-if="meta && meta.links">
         <div class="hidden md:flex">
           <a
             href="#"
@@ -67,7 +71,7 @@
               'text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700 border-indigo-500': link.active,
               'text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-400': !link.active,
             }"
-            class="-mt-px border-t-2 border-transparent pt-4 px-4 inline-flex items-center text-sm leading-5 font-medium focus:outline-none transition ease-in-out duration-150"
+            class="inline-flex items-center px-4 pt-4 -mt-px text-sm font-medium leading-5 transition duration-150 ease-in-out border-t-2 border-transparent focus:outline-none"
             v-html="link.label"
           >
           </a>
