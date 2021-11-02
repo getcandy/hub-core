4e04349da87af257f615ff1f6025557b8d65a500
Alec Ritson <hello@itsalec.co.uk>
2021-11-02 10:08:11 +0000

    Revert using tab component

--- hub-categories/src/pages/edit/CategoryAssociations.vue
+++ hub-categories/src/pages/edit/CategoryAssociations.vue
@@ -1,10 +1,18 @@
 <template>
   <div v-if="category">
-    <gc-tabs>
-      <gc-tab-item v-for="(tab, tabIndex) in tabs" :key="tabIndex" :label="tab.title">
-        <component :is="tab.component" :category="category" />
-      </gc-tab-item>
-    </gc-tabs>
+    <!-- Here we want to loop through any sections that have been hooked into -->
+    <div class="flex pl-4 text-gray-600 bg-gray-200">
+      <button
+        v-for="(item, tabIndex) in tabs"
+        :key="tabIndex"
+        :class="activeTab == tabIndex ? 'bg-white text-gray-600' : 'hover:bg-gray-300'"
+        class="px-6 py-4 text-sm border-none outline-none focus:border-none focus:outline-none "
+        @click="activeTab = tabIndex"
+      >
+        {{ item.title }}
+      </button>
+    </div>
+    <component :is="tab" :category="category" />
   </div>
 </template>
 
@@ -25,9 +33,16 @@ export default {
       }]
     }
   },
+  computed: {
+    tab () {
+      return this.tabs[this.activeTab].component
+    }
+  },
   mounted () {
     this.category = JSON.parse(JSON.stringify(this.$store.state.categories.model))
-    this.$nuxt.context.app.$hooks.callHook('categories.associations.tabs', this.tabs)
+    this.$nextTick(async () => {
+      this.$nuxt.context.app.$hooks.callHook('categories.associations.tabs', this.tabs)
+    })
   }
 }
 </script>
